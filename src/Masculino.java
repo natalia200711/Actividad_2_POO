37f751b5efed4dd891def5f62f5e2317330124f8
Natalia Cabrera Anaya <natalia.cabrera@est.iudigital.edu.co>
2025-05-24 13:47:54 -0500

    En las líneas 4 a 6 se implementó el constructor de la clase Masculino.

--- src/Masculino.java
+++ src/Masculino.java
@@ -1,2 +1,7 @@
 // Clase que hereda de Persona y representa a un individuo masculino
 public class Masculino {
+
+    // Constructor que automáticamente define el género como "Masculino"
+    public Masculino(String nombre, String apellido, int edad) {
+        super(nombre, apellido, "Masculino", edad);
+    }
