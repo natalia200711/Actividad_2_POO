fe1c4b4bc9d5046c3581e0a2874e26bf4aa0a541
Natalia Cabrera Anaya <natalia.cabrera@est.iudigital.edu.co>
2025-05-24 14:28:07 -0500

    En las líneas 4 a 6 se implementó el constructor de la clase Femenino.

--- src/Femenino.java
+++ src/Femenino.java
@@ -1,2 +1,7 @@
 // Clase que hereda de Persona y representa a un individuo femenino.
 public class Femenino extends Persona{
+
+    // Constructor que automáticamente define el género como "Femenino"
+    public Femenino(String nombre, String apellido, int edad) {
+        super(nombre, apellido, "Femenino", edad);
+    }
