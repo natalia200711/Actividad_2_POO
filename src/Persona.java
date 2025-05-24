5766c4ac07ee2344d8b611fa4eb5307fad4f55fc
Natalia Cabrera Anaya <natalia.cabrera@est.iudigital.edu.co>
2025-05-24 13:21:12 -0500

    En las líneas 3 a 6 se declararon los atributos privados nombre, apellido, genero y edad.

--- src/Persona.java
+++ src/Persona.java
@@ -1,2 +1,6 @@
 // Clase persona (abstracta para que nadie cree directamente una Persona)
 public abstract class Persona {
+    private String nombre;
+    private String apellido;
+    private String genero;
+    private int edad;
