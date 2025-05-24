8cf4c3b72198fa3a771ea1a11387536e4001ce7b
Natalia Cabrera Anaya <natalia.cabrera@est.iudigital.edu.co>
2025-05-24 13:33:09 -0500

    En las líneas 16 a 27 se agregaron los métodos públicos getNombre, getGenero y getEdad.

--- src/Persona.java
+++ src/Persona.java
@@ -12,3 +12,16 @@ public Persona(String nombre, String apellido, String genero, int edad) {
     this.genero = genero;
     this.edad = edad;
 }
+
+    // Métodos públicos (getters) para acceder a los atributos privados
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
